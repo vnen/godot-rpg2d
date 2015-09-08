de77d87c16f79860b17b38e7a6e14a58ce902b0b
George Marques <george@gmarqu.es>
2015-09-07 23:15:01 -0300

    Add dialogue frame with text

--- game/singletons/interaction.gd
+++ game/singletons/interaction.gd
@@ -2,4 +2,11 @@
 extends Node
 
 func say(what, who = null):
-	print(what, who)
+	for box in get_tree().get_nodes_in_group("dialogue"):
+		var text_box = box.get_node("dialog_text")
+		var text = ""
+		if(who):
+			text = "[bold]" + str(who) + "[/bold]: "
+		text += str(what)
+		text_box.set_bbcode(text)
+		box.show()
