460b82f490c96fcf636b2c53e913b3a8f8d122ba
George Marques <george@gmarqu.es>
2015-09-24 00:15:46 -0300

    Remove test function from item actions panel
    Also make it invisible by default.

--- game/gui/item_actions.gd
+++ game/gui/item_actions.gd
@@ -11,15 +11,6 @@ func _ready():
 	for node in get_node("ActionList").get_children():
 		node.add_color_override("font_color", disabled_color)
 		actions[node.get_name()] = false
-	set_process_input(true)
-
-func _input(event):
-	var miaction = "Give"
-	if(event.is_action("interact") and event.is_pressed() and !event.is_echo()):
-		if actions[miaction]:
-			disnable_action(miaction)
-		else:
-			enable_action(miaction)
 
 func enable_action(action):
 	assert(actions.has(action))
