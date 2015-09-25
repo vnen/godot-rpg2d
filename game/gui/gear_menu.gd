0b434dd0d5e3528047f84a0ffe55d9e498f9b739
George Marques <george@gmarqu.es>
2015-09-24 23:53:05 -0300

    Add cursor to gear menu
    - Make it pass events down the chain of controls.

--- game/gui/gear_menu.gd
+++ game/gui/gear_menu.gd
@@ -1,6 +1,14 @@
 
 extends PopupPanel
 
+var menu_actions = ["menu_up","menu_down","menu_left","menu_right","menu_select"]
+
 func _ready():
 	popup()
+	set_process_input(true)
 
+func _input(event):
+	for action in menu_actions:
+		if event.is_action(action):
+			get_tree().set_input_as_handled()
+			get_node("InventoryMenu/ItemsPanel")._input(event)
